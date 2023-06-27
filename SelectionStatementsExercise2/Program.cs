70673064fbe5a9e57b98b49aece1349c55ab52ac
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-06-26 20:37:43 -0400

    Initial Commit and Exercise 2 Completed

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            string userInput = Console.ReadLine();
+
+
+            switch (userInput)
+            {
+                case "Math":
+                case "math":
+
+                    Console.WriteLine($"{userInput} is fun to learn!");
+                     break;
+
+                case "Social Studies":
+                case "social studies":
+                    Console.WriteLine($"{userInput} was my favorite subject too!");
+                    break;
+
+                case "P.E.":
+                case "p.e.":
+                case "PE":
+                case "pe":
+
+                    Console.WriteLine($"{userInput} was a great subject!");
+                    break;
+
+                case "History":
+                case "history":
+                    Console.WriteLine($"{userInput} is important to learn!");
+                    break;
+
+                case "Science":
+                case "science":
+                    Console.WriteLine($"{userInput} is a blast to learn!");
+                    break;
+
+                default:
+                    Console.WriteLine($"{userInput} is amazing!");
+                    break;
+
+            }
         }
     }
 }
